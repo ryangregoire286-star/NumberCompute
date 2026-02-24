f2abf34c418afea0e15fdd359600e3ee8d9e63d6
BinaryConvertz <ryangregoire286@gmail.com>
2026-02-24 12:34:15 -0500

    Added Numbee-Struct

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,19 @@ pub fn get_rand_x(n2: f32) -> f32 {
     rand::random_range(1.0..n2)
 }
 
+#[allow(unused)]
+
+struct Numbers {
+    x: f32,
+    y: f32,
+}
+
 pub fn get_rand_y(n2: f32) -> f32 {
-    rand::random_range(1.0..n2)
+    let num = Box::new(Numbers { x: 1.0, y: n2 });
+
+    if n2 <= 1.0 as f32 {
+        rand::random_range(1.0..num.y)
+    } else {
+        rand::random_range(1.0..num.x)
+    }
 }
