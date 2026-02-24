97932ca9fe32989948fb069fbe161471c3210c9f
BinaryConvertz <ryangregoire286@gmail.com>
2026-02-24 11:20:09 -0500

    Added Math Functions to Code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod m_functions;
 mod objects;
 mod operators;
 
---
+++ src/m_functions.rs
@@ -0,0 +1,26 @@
+#[allow(unused)]
+
+struct MathFunc {
+    add: i32,
+    sub: i32,
+    mul: i32,
+    div: i32,
+}
+
+impl MathFunc {
+    pub fn _add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    pub fn _sub(a: i32, b: i32) -> i32 {
+        a - b
+    }
+
+    pub fn _mul(a: i32, b: i32) -> i32 {
+        a * b
+    }
+
+    pub fn _div(a: i32, b: i32) -> i32 {
+        a / b
+    }
+}
